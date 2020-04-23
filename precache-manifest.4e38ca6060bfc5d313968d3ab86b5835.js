d18182fb22fd12467875716ebfcafcd65b953d6e
aagrosse <aagrock81@yahoo.com>
2020-04-23 03:00:34 -0400

    Updates

---
+++ precache-manifest.4e38ca6060bfc5d313968d3ab86b5835.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fad7fae0016f3caab5a93fcc0c831c9e",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "11a739028e473fbe2938",
+    "url": "/React-Portfolio/static/css/main.a594e061.chunk.css"
+  },
+  {
+    "revision": "df2a6100443ea85a0270",
+    "url": "/React-Portfolio/static/js/2.aa0a8981.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Portfolio/static/js/2.aa0a8981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a739028e473fbe2938",
+    "url": "/React-Portfolio/static/js/main.aff86da2.chunk.js"
+  },
+  {
+    "revision": "1bdac82cd996ae1ecb8f",
+    "url": "/React-Portfolio/static/js/runtime-main.e19ca9a5.js"
+  },
+  {
+    "revision": "954eb84a7523595ae969abfd5b9b5840",
+    "url": "/React-Portfolio/static/media/AAG09457.954eb84a.JPG"
+  },
+  {
+    "revision": "c94552d2513350a42224f79c95ba1e09",
+    "url": "/React-Portfolio/static/media/AAG_Resume.c94552d2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/aagrosse/React-Portfolio/precache-manifest.18499dfca8e50e8aaffd6ceb85366c14.js"
+  "/React-Portfolio/precache-manifest.4e38ca6060bfc5d313968d3ab86b5835.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/aagrosse/React-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
